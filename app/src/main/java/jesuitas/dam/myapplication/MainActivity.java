e4102cc190468fa3b935c07a3e02e531585d8699
Alvaro Rodriguez <dam16.2020@gmail.com>
2020-10-05 10:43:06 +0200

    strings.xml, idiomas

--- app/src/main/java/jesuitas/dam/myapplication/MainActivity.java
+++ app/src/main/java/jesuitas/dam/myapplication/MainActivity.java
@@ -3,12 +3,21 @@ package jesuitas.dam.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView mshowHello;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mshowHello = findViewById(R.id.text4);  // Enlazamos el metodo mshowHello con un TextView
+    }
+
+    public void showToast(View view) {
+        mshowHello.setText(R.string.hello_text);
     }
 }
